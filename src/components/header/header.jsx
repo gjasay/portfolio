73bbb20e5d5b5a56ca3baadc7c10ca1842064c50
gjasay <gabeasay@gmail.com>
2024-02-07 12:41:30 -0500

    Updated color palette and lighting

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import { Float, OrbitControls, Text3D,  } from '@react-three/drei';
+import { Float, OrbitControls, Text3D, RoundedBox } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 import { Perf } from 'r3f-perf';
 import React from 'react';
@@ -7,16 +7,16 @@ import './header.css'
 const Header = () => {
     return (
         <div id='header'>
-            <Canvas  camera={{ fov: 80, position: [0, 0, 7]}}>
-                <Perf position="top-left" />
+            <Canvas  camera={{ fov: 80, position: [0, 0, 7]}} onClick={e => console.log('clicky clack')}>
+                {/* <Perf position="top-left" /> */}
                 <OrbitControls makeDefault />
                 <ambientLight />
-                <pointLight position={[10, 10, 10]} />
+                <pointLight position={[1, 1.5, 1]} />
                 <Float
-                speed={2.5}>
-                    <Text3D font={'fonts/optimer_bold.typeface.json'} position={[-6,0,0]} scale={[2,2,1]}> 
+                speed={5}>
+                    <Text3D font={'fonts/optimer_bold.typeface.json'} position={[-6,0,0]} scale={[2,2,1]} > 
                         Gabe Asay
-                        <meshStandardMaterial attach="material" color='#A5243D' />
+                        <meshStandardMaterial attach="material" color='#FFBA49' />
                     </Text3D>
                     <Box />
                 </Float>
@@ -31,8 +31,9 @@ function Box() {
         <mesh
         position={[0.25,0.65,-0.5]}
         scale={[1,1,1]}>
-            <boxGeometry args={[15, 4, 1]} />
-            <meshStandardMaterial attach="material" color='#B9CFD4' />
+            <RoundedBox args={[15, 4, 1]} radius={1.2}>
+                <meshStandardMaterial attach="material" color='#EF5B5B' />
+            </RoundedBox>
         </mesh>
     )
 }
--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -2,11 +2,12 @@ import { Float, OrbitControls, Text3D } from '@react-three/drei';
 import { Canvas} from '@react-three/fiber';
 import { Perf } from 'r3f-perf';
 import React from 'react';
+import './home.css';
 
 const Header = () => {
     return (
-        <div>
-            <p>Hello World!</p>
+        <div id='home'>
+            <h1>About me</h1>
         </div>
     );
 };
