28045dde20a7a44f27299523759e26fd006c20f7
gjasay <gabeasay@gmail.com>
2024-03-13 19:10:20 -0400

    Debugging build errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Main from './components/main/main'
 
 function App() {
@@ -10,11 +7,9 @@ function App() {
 
   return (
     <div id='app'>
-      <BrowserRouter>
         <div id='main'>
           <Main />
         </div>
-      </BrowserRouter>
     </div>
   )
 }
